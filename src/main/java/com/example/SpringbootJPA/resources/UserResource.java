057c0969b3f568a90729f96d84dd4500a84e17a4
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-10 16:41:03 -0300

    User service and repository findById and findAll

--- src/main/java/com/example/SpringbootJPA/resources/UserResource.java
+++ src/main/java/com/example/SpringbootJPA/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.example.SpringbootJPA.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringbootJPA.entities.User;
+import com.example.SpringbootJPA.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public  ResponseEntity<User> findAll(){
-			User u = new User(8l, "Jão", "jao@gmail.com", "999999999", "batatinha123");
-			return ResponseEntity.ok().body(u);
+	public  ResponseEntity<List<User>> findAll(){			
+		List<User> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public  ResponseEntity<User> findAById(@PathVariable Long id){			
+		User user = service.findById(id);		
+		return ResponseEntity.ok().body(user);
 	}
 }
