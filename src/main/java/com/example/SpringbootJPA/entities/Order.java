45e40062a7a587f24038b2c50715414ac7a1a817
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-11 19:29:10 -0300

    Enum orderStatus

--- src/main/java/com/example/SpringbootJPA/entities/Order.java
+++ src/main/java/com/example/SpringbootJPA/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.example.SpringbootJPA.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
@@ -24,16 +25,18 @@ public class Order implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	private Integer orderStatus;
 	
 	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "Client_id")
-	private User client;
+	private User client;	
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -51,6 +54,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
