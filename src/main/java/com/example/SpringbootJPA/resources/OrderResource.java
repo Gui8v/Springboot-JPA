296937eae936497cecb7c506a196547a466fe073
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-11 16:19:29 -0300

    Order entity part 2

---
+++ src/main/java/com/example/SpringbootJPA/resources/OrderResource.java
@@ -0,0 +1,33 @@
+package com.example.SpringbootJPA.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringbootJPA.entities.User;
+import com.example.SpringbootJPA.services.UserService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public  ResponseEntity<List<User>> findAll(){			
+		List<User> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public  ResponseEntity<User> findAById(@PathVariable Long id){			
+		User user = service.findById(id);		
+		return ResponseEntity.ok().body(user);
+	}
+}
