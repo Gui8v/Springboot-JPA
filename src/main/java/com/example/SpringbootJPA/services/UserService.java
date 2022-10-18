87c9f123b1bdb8c2d071b593dc829f7496f8de8b
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-18 18:24:29 -0300

    User update method

--- src/main/java/com/example/SpringbootJPA/services/UserService.java
+++ src/main/java/com/example/SpringbootJPA/services/UserService.java
@@ -31,6 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		User entity = repository.getReferenceById(id);
+		update(entity, user);
+		return repository.save(entity);
+	}
+
+	private void update(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 }
 
 
