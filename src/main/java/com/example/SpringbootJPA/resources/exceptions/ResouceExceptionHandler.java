ee68891e4962092beb1d8f12622075b3637c51ee
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-20 14:56:16 -0300

    Exception handler dor method delete

--- src/main/java/com/example/SpringbootJPA/resources/exceptions/ResouceExceptionHandler.java
+++ src/main/java/com/example/SpringbootJPA/resources/exceptions/ResouceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.SpringbootJPA.services.exceptions.DatabaseException;
 import com.example.SpringbootJPA.services.exceptions.ResoucerNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,8 +17,17 @@ public class ResouceExceptionHandler {
 	
 	@ExceptionHandler(ResoucerNotFoundException.class)
 	public ResponseEntity<StandardError> resouceNotFound(ResoucerNotFoundException e, HttpServletRequest request){
-		String error = "Resoucer not found";
+		String error = "Resouce not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(err);
