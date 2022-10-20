82e38e4c9fc43709701e33bbc40345f63b43377f
Gui8v <gui8v.rocha8@hotmail.com>
2022-10-20 15:03:45 -0300

    Exception handler for method update

--- src/main/java/com/example/SpringbootJPA/services/UserService.java
+++ src/main/java/com/example/SpringbootJPA/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -13,43 +14,50 @@ import com.example.SpringbootJPA.repositories.UserRepository;
 import com.example.SpringbootJPA.services.exceptions.DatabaseException;
 import com.example.SpringbootJPA.services.exceptions.ResoucerNotFoundException;
 
-@Service	
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ResoucerNotFoundException(id));
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-			
-		}catch (EmptyResultDataAccessException e) {
+
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResoucerNotFoundException(id);
-			
-		}catch (DataIntegrityViolationException e) {
+
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
-			
+
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User user) {
-		User entity = repository.getReferenceById(id);
-		update(entity, user);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			update(entity, user);
+			return repository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResoucerNotFoundException(id);
+		}
 	}
 
 	private void update(User entity, User user) {
@@ -58,5 +66,3 @@ public class UserService {
 		entity.setPhone(user.getPhone());
 	}
 }
-
-
